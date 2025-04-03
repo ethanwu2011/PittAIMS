af4d85e7614dbcc7d389ebafb5eb7c61b5359938
Ethan Wu <ethanwu2011@gmail.com>
2025-04-03 12:53:16 -0400

    Update configuration for custom domain pittaims.com

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react()],
-  base: mode === 'production' ? '/PittAIMS/' : '/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
