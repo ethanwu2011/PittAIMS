663e4f6134cd96ab490f1da03eaab7569a90785a
Ethan Wu <ethanwu2011@gmail.com>
2025-04-03 13:09:56 -0400

    Fix website configuration for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -26,4 +26,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
   publicDir: 'public'
-}));
+});
