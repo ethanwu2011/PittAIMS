56f55b4e5d8ca1564aff0159262f76ae057ebba4
Ethan Wu <ethanwu2011@gmail.com>
2025-04-03 01:24:03 -0400

    Update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react()],
-  base: './',
+  base: '/PittAIMS/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
